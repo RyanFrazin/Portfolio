044bf71aea8883bfe330cdad1b64b98524334f0b
Ryan Frazin <RyanFrazin@Gmail.com>
2025-08-06 15:21:13 -0700

    Add new pages, links

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get("/", (req, res) => {
     res.render("index.ejs")
 });
 
+app.get("/contact", (req, res) => {
+    res.render("contact.ejs");
+})
+
 app.listen(port, (req, res) => {
     console.log(`Server running on port ${port}`);
 }); 
